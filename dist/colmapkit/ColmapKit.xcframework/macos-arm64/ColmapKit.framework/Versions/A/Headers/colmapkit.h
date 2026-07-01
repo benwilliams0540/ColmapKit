1bd1cebfb5319a44d3bcb5b7dca76bc73401156f
Benjamin Williams <benwilliams0540@gmail.com>
2026-07-01 18:57:54 -0400

    build(colmapkit): package cancellation ABI xcframework

--- dist/colmapkit/ColmapKit.xcframework/macos-arm64/ColmapKit.framework/Versions/A/Headers/colmapkit.h
+++ dist/colmapkit/ColmapKit.xcframework/macos-arm64/ColmapKit.framework/Versions/A/Headers/colmapkit.h
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dae07d3f33138c26958ea1ac5a01a9d6e545a0692a6117ea464bf5405d2fda32
-size 4506
+oid sha256:2766b72d079d59c4c2726c496cf908addaf2bb0fa5001d40236bc469e8d48d56
+size 5224
